da47fcc4df9ba1d40da7c3fbae90b42b2ab1ca02
Tony Tran <117299835+tonytee88@users.noreply.github.com>
2023-05-09 13:35:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 function callScriptFunction() {
-google.script.run.withSuccessHandler(updateOutput).myScriptFunction();
-}
-
-function updateOutput(result) {
-var output = document.getElementById('output');
-output.innerText = result;
+    console.log("hello world");
 }
